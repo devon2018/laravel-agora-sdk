389461c67e6a7f3ec347b5be22049a7096ab6450
Devon Ray <devon@devonray.net>
2021-02-04 11:55:02 +0000

    Initial Commit

---
+++ src/AgoraServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Djinn\Agora;
+
+use Illuminate\Support\ServiceProvider;
+
+class AgoraServiceProvider extends ServiceProvider
+{
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+  }
+}
